0f4a9645c504cf5d359bfef2cc1437c79cfb6a23
Kushagra Saxena <kushagrasaxena061@gmail.com>
2024-11-16 20:10:50 +0530

    bugs fixed

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -6,8 +6,7 @@ import { ConvexHttpClient } from "convex/browser";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_uGGkoheXH-K3Ab3sYXFkQugwotS-bU3c5qHVPsHFnvpECFd00ZsLt49SocDjvc93",
+  secret:process.env.LIVEBLOCKS_SECRET_KEY!
 });
 
 export async function POST(request: Request) {
